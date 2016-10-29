ea06669e8c2717bb48fe85e10688bcf336a7049b
d1slike <eldustru@yandex.ru>
2016-10-29 20:34:39 +0300

    Init

---
+++ src/main/java/ru/disdev/L2LKApplication.java
@@ -0,0 +1,12 @@
+package ru.disdev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class L2LKApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(L2LKApplication.class, args);
+	}
+}
